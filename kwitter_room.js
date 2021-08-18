ab7ab816de0571f5a8a3bc4d1fe3e72b98818ce3
Anushree-Upadhyay <81459283+Anushree-Upadhyay@users.noreply.github.com>
2021-08-18 09:20:59 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,14 @@
 
-//ADD YOUR FIREBASE LINKS HERE
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCBbx1yJ8xpnFMECWL2BMowj5NrqAiigb4",
+    authDomain: "project-39d53.firebaseapp.com",
+    projectId: "project-39d53",
+    storageBucket: "project-39d53.appspot.com",
+    messagingSenderId: "884389201208",
+    appId: "1:884389201208:web:0e35008ed8ae67916e2dcd"
+  };
+  firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
